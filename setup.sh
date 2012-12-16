04745adb789e622a50005a51291f08aa4d2defa8
Lloyd Brookes <lloyd.brookes@gmail.com>
2012-12-16 12:41:56 +0000

    setup, git template config

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 mv ~/dotfiles ~/.dotfiles
-ln -s .dotfiles/.bash_profile ~/.bash_profile
-ln -s .dotfiles/.tm_properties ~/.tm_properties
-cp .gitconfig ~
+ln -s ~/.dotfiles/.bash_profile ~/.bash_profile
+ln -s ~/.dotfiles/.tm_properties ~/.tm_properties
+ln -s ~/.dotfiles/.git_template/ ~/.git_template
+ln -s ~/.dotfiles/.gitconfig/ ~/.gitconfig
