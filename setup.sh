ee31d06839340b9b6e61050bbd4d743ee180ea23
Lloyd Brookes <lloyd.brookes@gmail.com>
2016-07-31 17:06:52 +0100

    updates

--- setup.sh
+++ setup.sh
@@ -4,8 +4,6 @@ ln -fsv $(pwd)/tm_properties ~/.tm_properties
 rm -v ~/.git_template
 ln -sv $(pwd)/git_template ~/.git_template
 ln -fsv $(pwd)/gitconfig ~/.gitconfig
-ln -fsv $(pwd)/hgrc ~/.hgrc
-ln -fsv $(pwd)/hgignore_global ~/.hgignore_global
 ln -fsv $(pwd)/gitignore_global ~/.gitignore_global
 ln -fsv $(pwd)/gemrc ~/.gemrc
 ln -fsv $(pwd)/atom/config.cson ~/.atom/config.cson
